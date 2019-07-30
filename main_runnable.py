398e1ef3b0a2d44195ae15b774f75651695668fa
Luo <john.fang.luo@gmail.com>
2019-07-30 13:16:36 -0400

    Changed comment to be more accurate to the state of DuckDuckGo.

--- main_runnable.py
+++ main_runnable.py
@@ -3,7 +3,7 @@ import main_functions
 # Parameters to modify
 host_name = "http://pn108747.nist.gov:3680"
 search_engine = "https://www.google.com"  # i.e. 0
-# search_engine = "https://duckduckgo.com/" # i.e. 1. DON'T USE THIS FOR NOW (very low accuracy)
+# search_engine = "https://duckduckgo.com/" # i.e. 1. DON'T USE THIS FOR NOW (not very high accuracy)
 username="jfl2@localhost"
 password="jfl2_mysql"
 database_name="DBADSORPTION_SANDBOX"
