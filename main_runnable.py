bf828bdec4ca7e6364591024e7b3ba069b11a2bb
Luo <john.fang.luo@gmail.com>
2019-07-30 14:32:59 -0400

    Added more diagnostic print statements to demarcate the beginnings of the function calls.

--- main_runnable.py
+++ main_runnable.py
@@ -9,21 +9,26 @@ password="jfl2_mysql"
 database_name="DBADSORPTION_SANDBOX"
 
 # Function calls, actually runs the script
+print("Beginning authors2")
 main_functions.authors2(host_name)
 print("authors2 done")
 time.sleep(1)
 
+print("Beginning pairing_unknown_authors")
 main_functions.pairing_unknown_authors()
 print("pairing_unknown_authors done")
 time.sleep(1)
 
+print("Beginning scrape_authors")
 main_functions.scrape_authors(search_engine)
 print("scrape_authors done")
 time.sleep(1)
 
+print("Beginning comparing_scraped")
 main_functions.comparing_scraped()
 print("comparing_scraped done")
 time.sleep(1)
 
-main_functions.sql_database_writer(username, password, database_name)
-print("sql_database_writer done")
+# print("Beginning sql_database_writer")
+# main_functions.sql_database_writer(username, password, database_name)
+# print("sql_database_writer done")
